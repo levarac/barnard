47feb24fb1c88bae01876ad304cb43c79686861c
NAOE Kenichi <ken@naoe.tokyo>
2026-08-02 17:47:01 +0900

    fix(examples): handle event info hints in device tests

--- examples/android-native/app/src/androidTest/kotlin/org/levarac/barnard/example/devicelab/BarnardDeviceLabTest.kt
+++ examples/android-native/app/src/androidTest/kotlin/org/levarac/barnard/example/devicelab/BarnardDeviceLabTest.kt
@@ -184,6 +184,9 @@ class BarnardScannerDeviceLabTest {
                     is BarnardEvent.Error -> marker(
                         "BARNARD_EVT error code=${event.error.code} message=${event.error.message}",
                     )
+                    is BarnardEvent.EventInfoHint -> marker(
+                        "BARNARD_EVT event_info_hint name=${event.hint.eventInfo.eventDisplayName}",
+                    )
                 }
             }
         }
